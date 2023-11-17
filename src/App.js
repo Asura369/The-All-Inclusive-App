0df6a1c758c3fb5fce5171c7a0916bcd777973b0
kl3398 <kl3398@drexel.edu>
2023-11-16 22:52:10 -0500

    created basic login page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 // src/App.js
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Home from './components/Home'
 import TopNavigationBar from './components/TopNavigationBar'
 import PasswordGenerator from './components/Tools/PasswordGenerator'
+import LoginPage from './components/LoginPage'
 
 function App() {
+    const [user, setUser] = useState({})
+
     return (
         <Router>
             <div>
-                <TopNavigationBar />
+                <TopNavigationBar user={user} />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route
+                        path="/LoginPage"
+                        element={<LoginPage setUser={setUser} />}
+                    />
                     <Route
                         path="/PasswordGenerator"
                         element={<PasswordGenerator />}
--- src/components/TopNavigationBar.js
+++ src/components/TopNavigationBar.js
@@ -1,57 +1,28 @@
 // src/components/TopNavigationBar.js
-import React, { useEffect, useState } from 'react'
-import { jwtDecode } from 'jwt-decode'
+import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import './TopNavigationBar.css'
 
-const TopNavigationBar = () => {
-    const [user, setUser] = useState({})
-
-    function handleCallbackResponse(response) {
-        console.log('Enconded JWT ID Token: ', response.credential)
-        var userObject = jwtDecode(response.credential)
-        console.log('Decoded JWT Token:', userObject)
-        setUser(userObject)
-        document.getElementById('signInDiv').hidden = true
-    }
-
-    function handleSignOut(event) {
-        setUser({})
-        document.getElementById('signInDiv').hidden = false
-    }
-
-    useEffect(() => {
-        /* global google */
-        google.accounts.id.initialize({
-            client_id:
-                '701460506777-vtkbrv2li5frjo3l4th44mfoai1g2mth.apps.googleusercontent.com',
-            callback: handleCallbackResponse
-        })
-
-        google.accounts.id.renderButton(document.getElementById('signInDiv'), {
-            theme: 'outline',
-            size: 'large'
-        })
-
-        google.accounts.id.prompt()
-    }, [])
-
+const TopNavigationBar = ({ user }) => {
     return (
-        <Navbar bg="dark" variant="dark" className="top-navigation-bar">
+        <Navbar bg="primary" variant="light" className="top-navigation-bar">
             <Navbar.Brand href="/">Tools & Utilities</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/">
+                    Home
+                </Nav.Link>
             </Nav>
-            <Nav className="justify-content-end google-button">
-                <Nav.Link id="signInDiv"></Nav.Link>
-                {Object.keys(user).length !== 0 && (
-                    <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
-                )}
-                <Nav.Link id="signInDiv"></Nav.Link>
-                {user && (
-                    <div>
-                        <h3>{user.name}</h3>
-                    </div>
+            <Nav className="justify-content-end max-width">
+                {/* Conditionally render user's name or Login link */}
+                {user && Object.keys(user).length !== 0 ? (
+                    <Nav.Link as={Link} to="/LoginPage">
+                        Hi {user.name}!
+                    </Nav.Link>
+                ) : (
+                    <Nav.Link as={Link} to="/LoginPage">
+                        Login
+                    </Nav.Link>
                 )}
             </Nav>
         </Navbar>
