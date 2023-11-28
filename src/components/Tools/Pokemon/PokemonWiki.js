003d44dae7565fcd795ec73b0117c3a5f0b3932f
kl3398 <kl3398@drexel.edu>
2023-11-27 23:43:47 -0500

    added paging for pokemon list to cut down resources and added generated comments

--- src/components/Tools/Pokemon/PokemonWiki.js
+++ src/components/Tools/Pokemon/PokemonWiki.js
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom'
 import './Pokemon.css'
 
 const PokemonWiki = () => {
-    const [pokemonList, setPokemonList] = useState([])
-    const [activeGeneration, setActiveGeneration] = useState()
+    const [pokemonList, setPokemonList] = useState([]) // Store the list of Pokemon
+    const [activeGeneration, setActiveGeneration] = useState() // Store the active Pokemon generation
+    const [currentPage, setCurrentPage] = useState(1) // Store the current page number for pagination
+    const itemsPerPage = 20 // Number of Pokemon items to display per page
 
+    // Pokemon generation range dictionary
     const generation_dict = {
         1: [0, 151],
         2: [151, 251],
@@ -18,10 +21,13 @@ const PokemonWiki = () => {
         8: [809, 905]
     }
 
+    // Fetch Pokemon list based on the selected generation
     const fetchPokemonList = async (generation) => {
+        setCurrentPage(1) // Reset the current page when changing the generation
         const limit_value =
             generation_dict[generation][1] - generation_dict[generation][0]
         const offset_value = generation_dict[generation][0]
+
         try {
             const response = await fetch(
                 `https://pokeapi.co/api/v2/pokemon?limit=${limit_value}&offset=${offset_value}`
@@ -35,12 +41,22 @@ const PokemonWiki = () => {
         }
     }
 
+    // Extract Pokemon ID from the URL
     function getPokemonIdFromUrl(url) {
         const parts = url.split('/')
         const pokemon_id = parts[parts.length - 2]
         return pokemon_id
     }
 
+    // Pagination logic
+    const indexOfLastItem = currentPage * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentItems = pokemonList.slice(indexOfFirstItem, indexOfLastItem)
+
+    // Set the current page number
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
+    // JSX rendering
     return (
         <div className="pokemon-list-container">
             <h1>Pokemon List</h1>
@@ -61,7 +77,7 @@ const PokemonWiki = () => {
                 ))}
             </div>
             <ul className="pokemon-table">
-                {pokemonList.map((pokemon) => (
+                {currentItems.map((pokemon) => (
                     <li key={pokemon.name} className="pokemon-item">
                         <span className="pokemon-id">
                             {getPokemonIdFromUrl(pokemon.url)}
@@ -77,6 +93,22 @@ const PokemonWiki = () => {
                     </li>
                 ))}
             </ul>
+            <div className="pagination">
+                {Array.from(
+                    { length: Math.ceil(pokemonList.length / itemsPerPage) },
+                    (_, index) => (
+                        <button
+                            key={index + 1}
+                            onClick={() => paginate(index + 1)}
+                            className={
+                                currentPage === index + 1 ? 'active' : ''
+                            }
+                        >
+                            {index + 1}
+                        </button>
+                    )
+                )}
+            </div>
         </div>
     )
 }
